7a92c71e099c8acf90a814229ab13b968d386f9b
FRANCESCO BIAGINI <info@hanamisoftware.com>
2025-09-25 20:56:35 +0200

    rimosso Warm Up database. Inutile

--- js/subscribe.js
+++ js/subscribe.js
@@ -21,12 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
         // Pulizia URL dalla query string (evita ripetizione al refresh)
         window.history.replaceState({}, document.title, window.location.pathname);
     }
-   fetch('https://api.ninjagame.it/warmup', {
-        method: 'POST',
-        headers: {
-            'x-ninja-code': 'Ninj@G@me2025'
-        }
-    });
 });
 
     // Funzione di gestione del CAPTCHA Turnstile e del modulo di iscrizione
